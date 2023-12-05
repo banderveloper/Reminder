c45adbaa384f695f49c28c547dd9e964bdfdabfd
banderveloper <kpssdeveloper@gmail.com>
2023-12-05 02:48:06 +0200

    chore(web): add and test switching to dark theme

--- src/Reminder.Frontend/Reminder.Web/src/App.tsx
+++ src/Reminder.Frontend/Reminder.Web/src/App.tsx
@@ -1,10 +1,30 @@
 import './App.css'
+import {useEffect, useState} from "react";
 
 function App() {
 
+    const [theme, setTheme] = useState("light");
+
+    useEffect(() => {
+        if (theme === "dark") {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark")
+        }
+    }, [theme]);
+
+    const handleThemeSwitch = () => {
+        setTheme(theme === "dark" ? "light" : "dark");
+    };
+
     return (
         <>
-            <h1 className='text-3xl font-bold underline'>Hello world</h1>
+            <div className="h-screen bg-white dark:bg-black flex justify-center items-center">
+                <button className="bg-green-200 dark:bg-red-200 p-4 rounded-3xl" onClick={handleThemeSwitch}>
+                    Dark Mode
+                </button>
+            </div>
+
         </>
     );
 }
--- src/Reminder.Frontend/Reminder.Web/tailwind.config.js
+++ src/Reminder.Frontend/Reminder.Web/tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [
-      './index.html',
-      './src/**/*.{js,ts,jsx,tsx}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}'
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                light: ''
+            }
+        },
+    },
+    plugins: [],
 }
 
