bedf8a291a2063048551089dd36f8db4cde45ec1
banderveloper <kpssdeveloper@gmail.com>
2023-12-08 12:20:33 +0200

    feat(web): add demo auth form functionality

--- src/Reminder.Frontend/Reminder.Web/src/App.tsx
+++ src/Reminder.Frontend/Reminder.Web/src/App.tsx
@@ -1,25 +1,11 @@
 import './App.css'
-import {useAuthStore} from "./stores/useAuthStore.ts";
-import {useEffect} from "react";
+import AuthPage from "./pages/AuthPage/AuthPage.tsx";
 
 function App() {
 
-    const {isAuthenticated, signIn, errorCode} = useAuthStore();
-
-    useEffect(() => {
-        signIn({
-            username: 'admin',
-            fingerprint: 'admin',
-            password: 'admin'
-        })
-        // api.get('http://localhost:5000/temp/protected');
-
-    }, []);
-
     return (
         <>
-            <h1>Is authenticated: {isAuthenticated ? "Yes" : "No"}</h1>
-            <h2>Error code: {errorCode ? errorCode : "NULL"}</h2>
+            <AuthPage/>
         </>
     )
 }
--- src/Reminder.Frontend/Reminder.Web/src/components/AuthForm/AuthForm.tsx
+++ src/Reminder.Frontend/Reminder.Web/src/components/AuthForm/AuthForm.tsx
@@ -1,7 +1,32 @@
+import {useState} from "react";
+import {useAuthStore} from "../../stores/useAuthStore.ts";
+
 const AuthForm = () => {
+
+    const {isAuthenticated, signIn, errorCode} = useAuthStore();
+
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const fingerprint: string = 'web';
+
+    const handleSubmit = async () => {
+
+        await signIn({
+            username: username,
+            password: password,
+            fingerprint: fingerprint
+        });
+    }
+
     return (
         <div>
             <h1>Auth form</h1>
+            <input type="text" name="username" onChange={e => setUsername(e.target.value)}/>
+            <input type="text" name="password" onChange={e => setPassword(e.target.value)}/>
+            <button onClick={handleSubmit}>Go</button>
+
+            <h3>IsAuthenticated: {isAuthenticated ? "Yes" : "No"}</h3>
+            <h3>Error code: {errorCode ? errorCode : "NONE"}</h3>
         </div>
     );
 };
